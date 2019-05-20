3c4752193f65d6067482eb8d0d17731b6bbd935c
YA21 <ytik2013k@gmail.com>
2019-05-20 22:58:22 +0900

    add backend coded by flask

---
+++ backend/application.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+def create_app(app_name='FLASK-VUE'):
+    app = Flask(app_name,
+                static_folder = "./dist/static",
+                template_folder = "./dist")
+    app.config.from_object('backend.config.BaseConfig')
+
+    return app
