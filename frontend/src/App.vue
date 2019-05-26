705cd22435df56b1c324e45a4cbfb4979a7fc25f
YA21 <ytik2013k@gmail.com>
2019-05-26 17:18:19 +0900

    separate components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,9 @@
 <template>
   <div id=app>
-    <select v-model="selectedMonth">
-      <option v-for="month in months" :value=month.value :key=month.value>
-        {{ month.text }}
-      </option>
-    </select>
-    <input v-model="inputData" type="number">
-    <button class="btn btn-primary" v-on:click="updateElecData">Update</button>
+    <navigation-bar />
+    <input-form
+      @updateElecData="updateElecData"
+    />
     <button v-on:click="apiTest">apiTest</button>
     <line-chart
       class=chart
@@ -20,41 +17,27 @@
 
 <script>
 
+import NavigationBar from './components/NavigationBar'
+import InputForm from './components/InputForm'
 import LineChart from './components/LineChart'
 import axios from 'axios'
 
 export default {
   name: 'app',
-  components: { LineChart },
+  components: { NavigationBar, InputForm, LineChart },
   props: {},
   data () {
     return {
       elecData: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      inputData: 0,
-      lastUpdate: Date.now(),
-      selectedMonth: 0,
-      months: [
-        {text: 'Janualy', value: 1},
-        {text: 'Februaly', value: 2},
-        {text: 'March', value: 3},
-        {text: 'April', value: 4},
-        {text: 'May', value: 5},
-        {text: 'June', value: 6},
-        {text: 'July', value: 7},
-        {text: 'August', value: 8},
-        {text: 'September', value: 9},
-        {text: 'October', value: 10},
-        {text: 'November', value: 11},
-        {text: 'December', value: 12}
-      ]
+      lastUpdate: Date.now()
     }
   },
   methods: {
-    updateElecData: function () {
-      this.elecData[this.selectedMonth - 1] = parseInt(this.inputData)
+    updateElecData (selectedMonth, inputData) {
+      this.elecData[selectedMonth - 1] = parseInt(inputData)
       this.lastUpdate = Date.now()
     },
-    apiTest: function () {
+    apiTest () {
       const path = `http://localhost:5000/api/getelec`
       axios.get(path)
         .then(response => {
@@ -78,5 +61,11 @@ export default {
 
 #app {
   text-align: center;
+}
+
+.input-form {
+  display: flex;
+  width: 60%;
+  margin: auto;
 }
 </style>
---
+++ frontend/src/components/InputForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="input-form">
+    <select v-model="selectedMonth">
+      <option v-for="month in months" :value=month.value :key=month.value>
+        {{ month.text }}
+      </option>
+    </select>
+    <b-form-input v-model="inputData" type="number"></b-form-input>
+    <button class="btn btn-primary" v-on:click="updateElecData">Update</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data () {
+    return {
+      inputData: 0,
+      selectedMonth: 0,
+      months: [
+        {text: 'Janualy', value: 1},
+        {text: 'Februaly', value: 2},
+        {text: 'March', value: 3},
+        {text: 'April', value: 4},
+        {text: 'May', value: 5},
+        {text: 'June', value: 6},
+        {text: 'July', value: 7},
+        {text: 'August', value: 8},
+        {text: 'September', value: 9},
+        {text: 'October', value: 10},
+        {text: 'November', value: 11},
+        {text: 'December', value: 12}
+      ]
+    }
+  },
+  methods: {
+    updateElecData () {
+      this.$emit('updateElecData', this.selectedMonth, this.inputData)
+    }
+  }
+}
+</script>
+
+<style>
+.input-form {
+  display: flex;
+  width: 60%;
+  margin: auto;
+}
+</style>
